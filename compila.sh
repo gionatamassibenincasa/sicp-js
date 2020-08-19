993f34dbab32bfbd55482d1c9a4b47d5bcabb561
Gionata Massi <gionata.massi@savoiabenincasa.it>
2020-08-19 11:06:57 +0200

    Aggiunta la produzione del fomato DocBook nella compilazione

--- compila.sh
+++ compila.sh
@@ -12,6 +12,7 @@ do
             asciidoctor-pdf --verbose -r asciidoctor-diagram -r asciidoctor-mathematical -a mathematical-format=svg -a rouge-style=github -a attribute-missing=warn --failure-level=INFO -a pdf-stylesdir=resources/themes -a pdf-style=sicpit -a pdf-themesdir=resources/themes -a pdf-fontsdir=resources/fonts -a pdf-theme=sicpit sicp-js-it.adoc;;
         "pdf-fop" )
             echo "Compilazione in DookBook e PDF"
+            asciidoctor --verbose -b docbook5 -r asciidoctor-diagram -r asciidoctor-mathematical -a mathematical-format=svg -a rouge-style=github -a attribute-missing=warn --failure-level=INFO sicp-js-it.adoc
             fop -xml sicp-js-it.xml -xsl resources/fop/docbook-xsl/fo-pdf.xsl -pdf sicp-js-it-fop.pdf;;
          *) echo >&2 "Opzione non valida: $@"; exit 1;;
     esac
