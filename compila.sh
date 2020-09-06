d08a5399e3bb35f31a0123bcb1f89c2d4e3e5a4b
Gionata Massi <gionata.massi@savoiabenincasa.it>
2020-09-06 18:55:44 +0200

    Scambio

--- compila.sh
+++ compila.sh
@@ -6,7 +6,7 @@ do
     case "$opt" in
         "html" )
             echo "Compilazione in HTML"
-            asciidoctor --verbose -r asciidoctor-diagram -a  attribute-missing=warn --failure-level=INFO sicp-js-it.adoc;;
+	    asciidoctor -a revdate=$(cat revdate) -a revnumber=$(cat revnumber) -a revremark=$(cat revremark)  --verbose -r asciidoctor-diagram -a  attribute-missing=warn --failure-level=INFO sicp-js-it.adoc;;
         "pdf" )
             echo "Compilazione in PDF"
             asciidoctor-pdf --verbose -r asciidoctor-diagram -r asciidoctor-mathematical -a mathematical-format=svg -a rouge-style=github -a attribute-missing=warn --failure-level=INFO -a pdf-stylesdir=resources/themes -a pdf-style=sicpit -a pdf-themesdir=resources/themes -a pdf-fontsdir=resources/fonts -a pdf-theme=sicpit sicp-js-it.adoc;;
